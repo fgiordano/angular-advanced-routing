f357549d01bafa0745473877964832018df6c07f
Francesca Giordano <francesca020406@hotmail.com>
2017-11-11 15:25:43 -0500

    i have a routing error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService} from './contact-service.service';
+import { ContactOverviewComponent } from './contact-overview/contact-overview.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
 
+const routes: Routes = [
+  { path: '', component: ContactListComponent },
+  { path: 'contact/:id', component: ContactComponent,
+    children: [
+      { path: '', component: ContactOverviewComponent },
+      { path: 'edit', component: ContactEditComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactComponent,
+    ContactOverviewComponent,
+    ContactEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { ContactService } from '../contact-service.service';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css'],
+})
+export class ContactListComponent implements OnInit {
+
+  contacts: Array<any>;
+
+  constructor(
+    private router: Router,
+    private contactService: ContactService
+  ) {}
+
+  ngOnInit(){
+    this.contacts = this.contactService.getList();
+  }
+
+  viewDetails(id, param){
+    this.router.navigate(['contact', id], { queryParams: { foo: param }});
+  }
+
+}
